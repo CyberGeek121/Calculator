8b9a4477441735800c7a5c2cd542d6b93da46d71
CyberGeek121 <168563609+CyberGeek121@users.noreply.github.com>
2024-04-30 20:09:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// CALCULATOR PROGRAM
+
+const displat = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+        console.log(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+        console.log("Error");
+    }
+    
+}
